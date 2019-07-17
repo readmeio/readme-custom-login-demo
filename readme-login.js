07f594fcc26e700f56c9d860d84ce341fe9a5f9c
Marc Cuva <mjcuva@gmail.com>
2019-07-17 12:39:52 -0700

    Add more comments

--- readme-login.js
+++ readme-login.js
@@ -1,12 +1,17 @@
 var sign = require('jsonwebtoken').sign;
 var { URL } = require('url');
 
+// POST request to the login form
+// This should do all of the work to login and verify the user is valid
 module.exports = (req, res) => {
   var { email, password } = req.body;
 
   console.log('Use these credentials to log the user in somewhere:', { email, password });
 
+  // Validate email and password is a correct user, and get the user information
+
   // User being logged into ReadMe
+  // At this point we have a valid user, so we are sending them to readme
   var user = {
     name: email,
     email: email,
@@ -15,6 +20,8 @@ module.exports = (req, res) => {
     apiKey: 'test_123sldoih',
     
     isAdmin: true, // Optional: This being true will bypass the docs access control
+
+    // You can pass in any information here and use it in your documentation!
   };
 
   var jwt = sign(user, process.env.JWT_SECRET);
--- readme.md
+++ readme.md
@@ -14,6 +14,7 @@ Grab JWT secret from "User Connect" => "Magic Login Link" in the ReadMe Dash.
 JWT_SECRET=<your jwt secret> HUB_URL=<your hub url> npm start
 ```
 
+## Sync an example swagger file to your project
 Grab your api key from "Project Dashboard" => "API Key". 
 Sync the example swagger file to any project:
 
